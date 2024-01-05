2f64dbf7573436ebc579e9297fe178125520148b
Towhidul123 <excaliburfsh@gmail.com>
2024-01-05 14:33:54 +0600

    fixed some syntax

---
+++ frontend/src/Pages/Public/Cards/BiddingCard.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+const BiddingCard = ({ card }) => {
+
+    const { id, name, BrandName, Type, Price, ShortDescription, rating, image } = card;
+
+
+    return (
+        // <Link to={`/products/${id}`}>
+        <div>
+            <div className="flex items-center justify-center object-center ">
+                <div className="relative flex w-[312px] flex-col rounded-xl  ">
+                    <div className="relative  h-48 overflow-hidden rounded-xl ">
+                        <img className="object-cover w-full h-full " src={image} alt="" />
+                    </div>
+
+                    <div>
+                        <p className="block text-lg font-semibold text-center ">
+                            {name}
+                        </p>
+                    </div>
+
+                    <div className="mt-auto">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        // </Link>
+    );
+};
+
+export default BiddingCard;
