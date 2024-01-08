c5147cacddd81d1c4231111e8d9b81f5a5761593
Towhidul123 <excaliburfsh@gmail.com>
2024-01-08 20:00:08 +0600

    finalize nav bar

--- frontend/src/Pages/Public/NavBar.jsx
+++ frontend/src/Pages/Public/NavBar.jsx
@@ -43,7 +43,7 @@ const NavBar = () => {
                                                         />
                                                     </label>
                                                     <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                                                        <li><h2 className=""><FaUserAlt />{user.displayName}</h2></li>
+                                                        <li><h2 className="px-2 rounded-lg"><FaUserAlt />{user.displayName}</h2></li>
                                                         <Link to='/dashboard'> <li><button className="  px-2 rounded-lg" ><MdDashboard />DashBoard</button></li></Link>
                                                         <li><button className="  px-2 rounded-lg" onClick={handleLogOut}><FaSignOutAlt />Sign Out</button></li>
                                                     </ul>
@@ -105,13 +105,13 @@ const NavBar = () => {
     return (
         <nav>
             <div className="fixed top-0 w-full h-24 flex justify-between z-50 text-text  lg:py-5 md:px-20 px-5 py-4 ">
-                <div className="flex items-center">
+                <div className="flex items-center ">
                     <Logo></Logo>
                 </div>
                 <div className="lg:flex md:flex lg:flex-1 items-center justify-end font-normal hidden">
                     <div className="">
 
-                        <ul className="flex items-center gap-8 mr-16 font-medium font-heading text-[18px]">
+                        <ul className="flex items-center gap-2 md:gap-4 lg:gap-8 mr-16 font-medium font-heading text-[18px]">
                             <NavLink>
                                 <li className="hover:text-primary transition border-b-2 border-primary hover:border-primary cursor-pointer">Home</li>
                             </NavLink>
@@ -128,19 +128,19 @@ const NavBar = () => {
                             {
                                 user ? <>
 
-                                    <div className="gap-2 flex flex-col md:flex-row items-center hover:bg-primary  rounded-lg ">
-                                        <div className=" flex justify-center ">
+                                    <div className="gap-2 w-12 flex flex-col md:flex-row items-center hover:bg-primary  rounded-lg ">
+                                        <div className=" flex justify-center items-center ">
 
                                             <div className="dropdown dropdown-bottom dropdown-end text-text">
                                                 <label tabIndex={0} className=" m-1">
                                                     <img
-                                                        className="relative  inline-block w-12 aspect-square rounded-md object-cover object-center "
+                                                        className="relative  inline-block w-10 aspect-square rounded-md object-cover object-center "
                                                         alt="Image placeholder"
                                                         src={user.photoURL}
                                                     />
                                                 </label>
                                                 <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                                                    <li><h2 className=""><FaUserAlt />{user.displayName}</h2></li>
+                                                    <li><h2 className="px-2 rounded-lg"><FaUserAlt />{user.displayName}</h2></li>
                                                     <Link to='/dashboard'> <li><button className="  px-2 rounded-lg" ><MdDashboard />DashBoard</button></li></Link>
                                                     <li><button className="  px-2 rounded-lg" onClick={handleLogOut}><FaSignOutAlt />Sign Out</button></li>
                                                 </ul>
