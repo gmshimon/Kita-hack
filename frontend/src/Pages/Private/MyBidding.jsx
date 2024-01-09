6455957b343faf6bdcad77f44848fd0881f2f43d
Towhidul123 <excaliburfsh@gmail.com>
2024-01-10 00:50:31 +0600

    miscellaneous

--- frontend/src/Pages/Private/MyBidding.jsx
+++ frontend/src/Pages/Private/MyBidding.jsx
@@ -3,33 +3,42 @@ import useRole from "../../hooks/useRole";
 import { get } from "../../utilis/queries";
 
 const MyBidding = () => {
-  const { userRole } = useRole();
-  const [bids,setBids] = useState([])
+  //  const { userRole } = useRole();
+  const [bids, setBids] = useState([])
 
+  //   const [product, setProduct] = useState(null); 
+  //   useEffect(() => {
 
-  useEffect(()=>{
-    const callData = async()=>{
-      const result=  await get("products/user-bidding")
-    setBids(result.returnData)
+  //     fetch(`http://localhost:8000/api/v1/users/my-bid`)
+  //         .then(res => res.json())
+  //         .then(data => setProduct(data))
+  //         .catch(error => console.error('Error fetching data:', error));
+  // }, []);
+
+  useEffect(() => {
+    const callData = async () => {
+      const result = await get("users/my-bid")
+      setBids(result.returnData)
     }
     callData()
-  },[])
+  }, [])
   console.log(bids)
-  if (userRole === "seller") {
-    return (
-      <>
-        <h1>SELLER BIDDINGS</h1>
-      </>
-    );
-  }
 
-  if (userRole === "buyer") {
-    return (
-      <>
-        <h1>BUYER BIDDINGS</h1>
-      </>
-    );
-  }
+  // if (userRole === "seller") {
+  //   return (
+  //     <>
+  //       <h1>SELLER BIDDINGS</h1>
+  //     </>
+  //   );
+  // }
+
+  // if (userRole === "buyer") {
+  //   return (
+  //     <>
+  //       <h1>BUYER BIDDINGS</h1>
+  //     </>
+  //   );
+  // }
 };
 
 export default MyBidding;
